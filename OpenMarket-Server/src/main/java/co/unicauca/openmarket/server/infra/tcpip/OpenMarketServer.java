149a8efe1fc7230c9b5e2594a498d9a101d86ebd
BaironCamiloDiaz <bayrondiaz@unicauca.edu.co>
2023-05-17 13:26:36 -0500

    first commit

---
+++ OpenMarket-Server/src/main/java/co/unicauca/openmarket/server/infra/tcpip/OpenMarketServer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unicauca.openmarket.server.infra.tcpip;
+
+import co.unicauca.strategyserver.infra.ServerSocketMultiThread;
+import co.unicauca.travelagency.server.access.CustomerRepositoryImplArrays;
+import co.unicauca.openmarket.server.domain.services.CustomerService;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author ahurtado
+ */
+public class OpenMarketServer {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el puerto de escucha");
+        int port = teclado.nextInt();
+        ServerSocketMultiThread myServer = new ServerSocketMultiThread(port);
+        OpenMarketHandler myHandler = new OpenMarketHandler();
+        myHandler.setService(new CustomerService(new CustomerRepositoryImplArrays()));
+        myServer.setServerHandler(myHandler);
+        myServer.startServer();
+    }
+    
+}
